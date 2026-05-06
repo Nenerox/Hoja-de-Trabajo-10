124a4d6c6f35f94f9ffbb66382bd07df7509b7c5
Nenerox <andres.ps0308@gmail.com>
2026-05-06 15:00:39 -0600

    Se agrego el menu de opciones

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,83 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        int opcion = -1;
         TxtReader reader = new TxtReader();
         String[] caminos = reader.readFile("src\\guategrafo.txt", "\n");
+        Scanner scanner = new Scanner(System.in);
 
         Digrafo<String, Integer> digrafo = new Digrafo<>(caminos.length * 2);
 
-        // Agregar vértices y aristas al grafo
-        for (int i = 0; i < caminos.length; i++) {
-            String[] partes = caminos[i].split(" ");
-            digrafo.addVertice(partes[0].strip());
-            digrafo.addVertice(partes[1].strip());
-            digrafo.addArista(partes[0].strip(), partes[1].strip(), Integer.parseInt(partes[2].strip()));
+        try {
+            // Agregar vértices y aristas al grafo
+            for (int i = 0; i < caminos.length; i++) {
+                String[] partes = caminos[i].split(" ");
+                digrafo.addVertice(partes[0].strip());
+                digrafo.addVertice(partes[1].strip());
+                digrafo.addArista(partes[0].strip(), partes[1].strip(), Integer.parseInt(partes[2].strip()));
+            }
+        } catch (Exception e) {
+            System.err.println("Error al procesar el grafo: " + e.getMessage());
         }
 
-        digrafo.imprimirMatriz();
+        System.out.println("Grafo creado con éxito.");
+
+        while (opcion !=0) {
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Imprimir matriz de adyacencia \n 2. Eliminar ciudad \n 3. Eliminar camino \n 4.Agregar camino \n 5. Calcular ruta más corta \n 6. Calcular centro \n 0. Salir");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Consumir el salto de línea
+            switch (opcion) {
+                case 1:
+                    System.out.println("Matriz de adyacencia del digrafo:");
+                    digrafo.imprimirMatriz();
+                    break;
+
+                case 2:
+                    System.out.print("Ingrese el nombre de la ciudad a eliminar: ");
+                    String ciudadEliminar = scanner.nextLine().strip();
+                    digrafo.removeVertice(ciudadEliminar);
+                    break;
+                    
+                case 3:
+                    System.out.print("Ingrese el nombre de la primera ciudad: ");
+                    String ciudad1 = scanner.nextLine().strip();
+                    System.out.print("Ingrese el nombre de la segunda ciudad: ");
+                    String ciudad2 = scanner.nextLine().strip();
+                    digrafo.removeArista(ciudad1, ciudad2);
+                    break;
+
+                case 4:
+                    System.out.print("Ingrese el nombre de la primera ciudad: ");
+                    String ciudad1Agregar = scanner.nextLine().strip();
+                    System.out.print("Ingrese el nombre de la segunda ciudad: ");
+                    String ciudad2Agregar = scanner.nextLine().strip();
+                    System.out.print("Ingrese el peso del camino: ");
+                    int peso = scanner.nextInt();
+                    scanner.nextLine(); // Consumir el salto de línea
+                    digrafo.addArista(ciudad1Agregar, ciudad2Agregar, peso);
+                    break;
+
+                case 5:
+                    System.out.print("Ingrese el nombre de la primera ciudad: ");
+                    String origen = scanner.nextLine().strip();
+                    System.out.print("Ingrese el nombre de la segunda ciudad: ");
+                    String destino = scanner.nextLine().strip();
+                    //digrafo.Floyd(origen, destino);
+                    break;
+                case 6:
+                    System.out.println("La ciudad central es:");
+                    //digrafo.centro();
+                case 0:
+                    System.out.println("Saliendo");
+                    break;
+
+                default:
+                    throw new AssertionError();
+            }
+
+        }
+
     }
 }
