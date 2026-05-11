66199b14b19657f86bdf0ff801a013c481069437
Nenerox <andres.ps0308@gmail.com>
2026-05-11 17:09:06 -0600

    Se ordeno mejor el menu de opciones

--- src/Main.java
+++ src/Main.java
@@ -25,47 +25,64 @@ public class Main {
 
         while (opcion !=0) {
             System.out.println("Seleccione una opción:");
-            System.out.println(" 1. Eliminar ciudad \n 2. Eliminar camino \n 3. Agregar camino \n 4. Calcular ruta más corta \n 5. Calcular centro \n 6. Imprimir matriz de distancias \n 0. Salir");
+            System.out.println(" 1. Calcular ruta mas corta \n 2. Ciudad central \n 3. Modificar grafo \n 4. Mostrar matriz \n 0. Salir");
             opcion = scanner.nextInt();
             scanner.nextLine(); // Consumir el salto de línea
             switch (opcion) {
                 case 1:
-                    System.out.print("Ingrese el nombre de la ciudad a eliminar: ");
-                    String ciudadEliminar = scanner.nextLine().strip();
-                    digrafo.removeVertice(ciudadEliminar);
-                    break;
-                    
-                case 2:
-                    System.out.print("Ingrese el nombre de la primera ciudad: ");
-                    String ciudad1 = scanner.nextLine().strip();
-                    System.out.print("Ingrese el nombre de la segunda ciudad: ");
-                    String ciudad2 = scanner.nextLine().strip();
-                    digrafo.removeArista(ciudad1, ciudad2);
-                    break;
-
-                case 3:
-                    System.out.print("Ingrese el nombre de la primera ciudad: ");
-                    String ciudad1Agregar = scanner.nextLine().strip();
-                    System.out.print("Ingrese el nombre de la segunda ciudad: ");
-                    String ciudad2Agregar = scanner.nextLine().strip();
-                    System.out.print("Ingrese el peso del camino: ");
-                    int peso = scanner.nextInt();
-                    scanner.nextLine(); // Consumir el salto de línea
-                    digrafo.addArista(ciudad1Agregar, ciudad2Agregar, peso);
-                    break;
-
-                case 4:
                     System.out.print("Ingrese el nombre de la primera ciudad: ");
                     String origen = scanner.nextLine().strip();
                     System.out.print("Ingrese el nombre de la segunda ciudad: ");
                     String destino = scanner.nextLine().strip();
                     digrafo.RutaMasCorta(origen, destino);
                     break;
-                case 5:
+                    
+                case 2:
                     System.out.println("La ciudad central es: " + digrafo.calcularCentro());
                     break;
-                case 6:
-                    digrafo.imprimirMatriz();
+
+                case 3:
+                    System.out.println("Seleccione una opción:");
+                    System.out.println(" 1. Eliminar camino \n 2. Agregar camino");
+                    int subOpcion = scanner.nextInt();
+                    scanner.nextLine(); 
+                    switch (subOpcion) {
+                        case 1:
+                            System.out.print("Ingrese el nombre de la primera ciudad: ");
+                            String ciudad1 = scanner.nextLine().strip();
+                            System.out.print("Ingrese el nombre de la segunda ciudad: ");
+                            String ciudad2 = scanner.nextLine().strip();
+                            digrafo.removeArista(ciudad1, ciudad2);
+                            break;
+                    
+                        case 2:
+                            System.out.print("Ingrese el nombre de la primera ciudad: ");
+                            String ciudad1Agregar = scanner.nextLine().strip();
+                            System.out.print("Ingrese el nombre de la segunda ciudad: ");
+                            String ciudad2Agregar = scanner.nextLine().strip();
+                            System.out.print("Ingrese el peso del camino: ");
+                            int peso = scanner.nextInt();
+                            scanner.nextLine();
+                            digrafo.addArista(ciudad1Agregar, ciudad2Agregar, peso);
+                            break;
+                        default:
+                            throw new AssertionError("Opción no válida");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Seleccione una opción:");
+                    System.out.println(" 1. Matriz de adyacencia \n 2. Matriz de distancias");
+                    int tipo = scanner.nextInt();
+                    if (tipo == 1) {
+                        System.out.println("Matriz de adyacencia:");
+                        digrafo.imprimirMatriz("adyacencia");
+                    } else if (tipo == 2) {
+                        System.out.println("Matriz de distancias:");
+                        digrafo.imprimirMatriz("distancia");
+                    } else {
+                        System.out.println("Opción no válida");
+                    }
                     break;
                 case 0:
                     System.out.println("Saliendo");
